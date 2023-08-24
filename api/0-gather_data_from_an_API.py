d25c923c2c0d3470ad0c1f3f968f50df127ba61d
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-08-24 15:19:49 -0700

    task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" given employee ID, returns information about his/her TODO list progress."""
+
+import requests
+import sys
+
+
+def get_employee_todo_progress(employee_id):
+
+    base_url = 'https://jsonplaceholder.typicode.com'
+
+    user_response = requests.get(f'{base_url}/users/{employee_id}')
+    todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
+
+    user_data = user_response.json()
+    todos_data = todos_response.json()
+
+    employee_name = user_data['name']
+    total_tasks = len(todos_data)
+    done_tasks = sum(1 for todo in todos_data if todo['completed'])
+
+    print(f"Employee {employee_name} is done with tasks\
+        ({done_tasks}/{total_tasks}):")
+
+    for todo in todos_data:
+        if todo['completed']:
+            print(f"\t{todo['title']}")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+    else:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
